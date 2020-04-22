7f896ec6d9a1b96d6930f6e216fe8bb306ff71fe
Aresiel <25839246+Aresiel@users.noreply.github.com>
2020-04-22 21:36:13 +0200

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,12 +1,12 @@
-function generate(){
+function generate() {
     fetch('https://random-word-api.herokuapp.com/word?number=3')
-  .then(response => response.json())
-	.then(json => {
-  let password = ""
-	json.forEach(word => {
-	password += word.charAt(0).toUpperCase() + word.slice(1);
-})
-password += Math.floor(Math.random()*999)
-	return password
-})
+        .then(response => response.json())
+        .then(json => {
+            let password = ""
+            json.forEach(word => {
+                password += word.charAt(0).toUpperCase() + word.slice(1);
+            })
+            password += Math.floor(Math.random() * 999)
+            document.getElementById('password').value = password
+        })
 }
